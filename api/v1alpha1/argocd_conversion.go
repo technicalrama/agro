jparsai/argocd-operator

feat: Add support for Aggregated ClusterRole for cluster scoped instances
Signed-off-by: Jayendra Parsai <jparsai@jparsai-thinkpadp1gen4i.remote.csb>

api/v1alpha1/argocd_conversion.go
@@ -94,6 +94,7 @@ func (src *ArgoCD) ConvertTo(dstRaw conversion.Hub) error {
 	dst.Spec.Version = src.Spec.Version
 	dst.Spec.Banner = (*v1beta1.Banner)(src.Spec.Banner)
 	dst.Spec.DefaultClusterScopedRoleDisabled = src.Spec.DefaultClusterScopedRoleDisabled
+	dst.Spec.AggregatedClusterRoles = src.Spec.AggregatedClusterRoles
 
 	// Status conversion
 	dst.Status = v1beta1.ArgoCDStatus(src.Status)
@@ -162,6 +163,7 @@ func (dst *ArgoCD) ConvertFrom(srcRaw conversion.Hub) error {
 	dst.Spec.Version = src.Spec.Version
 	dst.Spec.Banner = (*Banner)(src.Spec.Banner)
 	dst.Spec.DefaultClusterScopedRoleDisabled = src.Spec.DefaultClusterScopedRoleDisabled
+	dst.Spec.AggregatedClusterRoles = src.Spec.AggregatedClusterRoles
 
 	// Status conversion
 	dst.Status = ArgoCDStatus(src.Status)

api/v1alpha1/argocd_types.go
@@ -829,6 +829,9 @@ type ArgoCDSpec struct {
 
 	// DefaultClusterScopedRoleDisabled will disable creation of default ClusterRoles for a cluster scoped instance.
 	DefaultClusterScopedRoleDisabled bool `json:"defaultClusterScopedRoleDisabled,omitempty"`
+
+	// AggregatedClusterRoles will allow users to have aggregated ClusterRoles for a cluster scoped instance.
+	AggregatedClusterRoles bool `json:"aggregatedClusterRoles,omitempty"`
 }
 
 // ArgoCDStatus defines the observed state of ArgoCD

api/v1beta1/argocd_types.go
@@ -860,6 +860,9 @@ type ArgoCDSpec struct {
 
 	// DefaultClusterScopedRoleDisabled will disable creation of default ClusterRoles for a cluster scoped instance.
 	DefaultClusterScopedRoleDisabled bool `json:"defaultClusterScopedRoleDisabled,omitempty"`
+
+	// AggregatedClusterRoles will allow users to have aggregated ClusterRoles for a cluster scoped instance.
+	AggregatedClusterRoles bool `json:"aggregatedClusterRoles,omitempty"`
 }
 
 // ArgoCDStatus defines the observed state of ArgoCD

technicalrama/agro

api/v1alpha1/argocd_conversion.go
@@ -94,6 +94,7 @@ func (src *ArgoCD) ConvertTo(dstRaw conversion.Hub) error {
 	dst.Spec.Version = src.Spec.Version
 	dst.Spec.Banner = (*v1beta1.Banner)(src.Spec.Banner)
 	dst.Spec.DefaultClusterScopedRoleDisabled = src.Spec.DefaultClusterScopedRoleDisabled
+	dst.Spec.AggregatedClusterRolesEnabled = src.Spec.AggregatedClusterRolesEnabled
 
 	// Status conversion
 	dst.Status = v1beta1.ArgoCDStatus(src.Status)
@@ -162,6 +163,7 @@ func (dst *ArgoCD) ConvertFrom(srcRaw conversion.Hub) error {
 	dst.Spec.Version = src.Spec.Version
 	dst.Spec.Banner = (*Banner)(src.Spec.Banner)
 	dst.Spec.DefaultClusterScopedRoleDisabled = src.Spec.DefaultClusterScopedRoleDisabled
+	dst.Spec.AggregatedClusterRolesEnabled = src.Spec.AggregatedClusterRolesEnabled
 
 	// Status conversion
 	dst.Status = ArgoCDStatus(src.Status)

api/v1alpha1/argocd_types.go
@@ -829,6 +829,9 @@ type ArgoCDSpec struct {
 
 	// DefaultClusterScopedRoleDisabled will disable creation of default ClusterRoles for a cluster scoped instance.
 	DefaultClusterScopedRoleDisabled bool `json:"defaultClusterScopedRoleDisabled,omitempty"`
+
+	// AggregatedClusterRolesEnabled will allow users to have aggregated ClusterRoles for a cluster scoped instance.
+	AggregatedClusterRolesEnabled bool `json:"aggregatedClusterRolesEnabled,omitempty"`
 }
 
 // ArgoCDStatus defines the observed state of ArgoCD

api/v1beta1/argocd_types.go
@@ -860,6 +860,9 @@ type ArgoCDSpec struct {
 
 	// DefaultClusterScopedRoleDisabled will disable creation of default ClusterRoles for a cluster scoped instance.
 	DefaultClusterScopedRoleDisabled bool `json:"defaultClusterScopedRoleDisabled,omitempty"`
+
+	// AggregatedClusterRolesEnabled will allow users to have aggregated ClusterRoles for a cluster scoped instance.
+	AggregatedClusterRolesEnabled bool `json:"aggregatedClusterRolesEnabled,omitempty"`
 }
 
 // ArgoCDStatus defines the observed state of ArgoCD